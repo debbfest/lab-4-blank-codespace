d583509489208241ce4f2bd6580c02680a5ec671
debbfest <148581658+debbfest@users.noreply.github.com>
2023-11-09 04:53:51 +0000

    End of sublab 004

--- devdojo.sh
+++ devdojo.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-name="DevDojo"
-greeting="Hello"
 
-echo "$greeting $name"
+echo "Hello there" $1
 
+# $1 : first parameter
+
+echo "Hello there" $2
+
+# $2 : second parameter
+
+echo "Hello there" $@
+
+# $@ : all
